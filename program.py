83c6f933ace1d4032100e85cb775ee5aa26782e4
geraldwogan <geraldwogan@gmail.com>
2022-04-01 13:40:33 +0100

    Initial API testing

--- program.py
+++ program.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import re
+import requests
 
 # Download media tracking file as .xlsx and convert to .csv
 read_file = pd.read_excel('data/2021 GW Media Tracking.xlsx', sheet_name='media_tracking', engine='openpyxl')
@@ -12,8 +13,13 @@ albums = read_file[read_file["Medium"]=="Album"]
 albums["master_id"] = 'm' + albums["Standardised ID"].str.extract(r'\/([0-9]{6,7})-')
 
 # Create tidier df with just the relevant info
-albums = albums.loc[:,["Num", "Title","Artist", "Date Started", "Date Finished","Days", "Month", "master_id"]]
+albums = albums.loc[:,["Num", "Title", "Creator/Season", "Date Started", "Date Finished","Days", "Month", "master_id"]]
 print(albums.head())
 
 # 1. Get info (Genre, Release Year, Album Cover) from API using master id. 
+test_id = albums.iloc[0]["master_id"]
+# print(test_id)
+response = requests.get(f'https://api.discogs.com/database/search?q=Nirvana&token=abcxyz123456 --user-agent "allmusic-artist-association/1.0 +https://github.com/geraldwogan/allmusic-artist-association"')
+print(response.status_code)
+print(response.json())
 
