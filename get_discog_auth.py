c8027e4fbc5a62a49f3eabded2899a42b0ff9e76
geraldwogan <geraldwogan@gmail.com>
2022-04-02 23:11:01 +0100

    Acquired Discogs API access tokens

--- get_discog_auth.py
+++ get_discog_auth.py
@@ -1,29 +1,11 @@
-import pandas as pd
 import requests
 import oauth2
 import sys
 from urllib import request
 from urllib.parse import parse_qsl
 from urllib.parse import urlparse
-# Download media tracking file as .xlsx and convert to .csv
-read_file = pd.read_excel('data/2021 GW Media Tracking.xlsx', sheet_name='media_tracking', engine='openpyxl')
 
-# Extract relevant content (master id from ID column of rows labeled 'Album')
-albums = read_file[read_file["Medium"]=="Album"]
-albums["master_id"] = 'm' + albums["Standardised ID"].str.extract(r'\/([0-9]{6,7})-')
-
-# Create tidier df with just the relevant info
-albums = albums.loc[:,["Num", "Title", "Creator/Season", "Date Started", "Date Finished","Days", "Month", "master_id"]]
-print(albums.head())
-
-# 1. Get info (Genre, Release Year, Album Cover) from API using master id. 
-test_id = albums.iloc[0]["master_id"]
-
-# Consumer Key & Secret from https://www.discogs.com/settings/developers
-parameters = {
-    "Consumer Key": "MyVgXQbTbpSHOQHuqWGL",
-    "Consumer Secret": "mQrzxHuAPZQMOQiqGWTboVAPWUamxrqJ"
-} 
+# Use this repo to follow along: https://github.com/jesseward/discogs-oauth-example/blob/master/discogs_example.py
 
 consumer_key = 'MyVgXQbTbpSHOQHuqWGL'
 consumer_secret = 'mQrzxHuAPZQMOQiqGWTboVAPWUamxrqJ'
@@ -74,10 +56,3 @@ print(f'    * oauth_token        = {access_token["oauth_token"]}')
 print(f'    * oauth_token_secret = {access_token["oauth_token_secret"]}')
 print(' Authentication complete. Future requests must be signed with the above tokens.')
 print()
-
-
-
-
-response = requests.get(f'http://api.discogs.com/oauth/request_token?key=MyVgXQbTbpSHOQHuqWGL&secret=mQrzxHuAPZQMOQiqGWTboVAPWUamxrqJ --user-agent "album-artwork-association/1.0 +https://github.com/geraldwogan/album-artwork-association"') # failing currently
-print(response.status_code)
-print(response.json())
