b42dd271577597b8532adbd59ba7703cb047005e
geraldwogan <geraldwogan@gmail.com>
2022-04-03 23:33:23 +0100

    Tidy data-cleaning functionality into its own function

--- program.py
+++ program.py
@@ -3,21 +3,25 @@ import json
 import oauth2
 import sys
 
-
 # Download media tracking file as .xlsx and convert to .csv
 read_file = pd.read_excel('data/2021 GW Media Tracking.xlsx', sheet_name='media_tracking', engine='openpyxl')
 
-# Extract relevant content (master id from ID column of rows labeled 'Album')
-albums = read_file[read_file["Medium"]=="Album"]
-albums["master_id"] = 'm' + albums["Standardised ID"].str.extract(r'\/([0-9]{6,7})-')
+def data_cleaning(read_file):
+    
+    # Extract relevant content (master id from ID column of rows labeled 'Album')
+    albums = read_file[read_file["Medium"]=="Album"]
+    albums["master_id"] = 'm' + albums["Standardised ID"].str.extract(r'\/([0-9]{6,7})-')
 
-# Create 'search friendly' columns for artist and album
-albums['search_artist'] = albums['Creator/Season'].str.replace(' ', '+')
-albums['search_album'] = albums['Title'].str.replace(' ', '+')
+    # Create 'search friendly' columns for artist and album
+    albums['search_artist'] = albums['Creator/Season'].str.replace(' ', '+')
+    albums['search_album'] = albums['Title'].str.replace(' ', '+')
 
-# Create tidier df with just the relevant info
-albums = albums.loc[:,["Num", "Title", "Creator/Season", "Date Started", "Date Finished","Days", "Month", "master_id", "search_artist", "search_album"]]
-print(albums.head())
+    # Create tidier df with just the relevant info
+    albums = albums.loc[:,["Num", "Title", "Creator/Season", "Date Started", "Date Finished","Days", "Month", "master_id", "search_artist", "search_album"]]
+    print(albums.head())
+    return albums
+
+albums = data_cleaning(read_file)
 
 # 1. Get info (Genre, Release Year, Album Cover) from API using master id. 
 test_id = albums.iloc[0]["master_id"]
@@ -26,7 +30,6 @@ test_search_album = albums.iloc[0]["search_album"]
 print(f'test_id: "{test_id}".')
 print(f'test_search_artist: "{test_search_artist}".')
 print(f'test_search_album: "{test_search_album}".')
-
 
 # credentials
 json_file = open("discogs_auths.json")
