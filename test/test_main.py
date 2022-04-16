288925ed9bf1aa58fc6dd89b25249936a079c682
geraldwogan <geraldwogan@gmail.com>
2022-04-17 00:24:55 +0100

    Add test for get_secrets

--- test/test_main.py
+++ test/test_main.py
@@ -1,5 +1,21 @@
 import unittest
-
+import main
 # Add tests for main.py
 # - get_secrets()
 # - get_auth_client()
+
+class TestMain(unittest.TestCase):
+
+    def test_get_secrets(self):
+
+            secrets = main.get_secrets()
+            self.assertIsNotNone(secrets)
+            length = len(secrets)
+            assertEquals(secrets, 5)
+
+            
+
+if __name__ == '__main__':
+
+        unittest.main()
+
