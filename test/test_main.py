3c6610b9e6294181d8e6039fb09c575b419209e1
geraldwogan <geraldwogan@gmail.com>
2022-04-18 00:18:49 +0100

    Fix indentation + code clean-up

--- test/test_main.py
+++ test/test_main.py
@@ -1,23 +1,17 @@
 import unittest
 import main
-# Add tests for main.py
-# - get_secrets()
-# - get_auth_client()
 
 class TestMain(unittest.TestCase):
 
     def test_get_secrets(self):
-
-            secrets = main.get_secrets()
-            self.assertIsNotNone(secrets)
-            length = len(secrets)
-            assertEquals(secrets, 5)
+        secrets = main.get_secrets()
+        self.assertIsNotNone(secrets)
+        length = len(secrets)
+        assertEquals(secrets, 5)
 
     def test_setup_auth_client(self, secrets):
         client = setup_auth_client(secrets)
         self.assertIsNotNone(client)
 
-
 if __name__ == '__main__':
         unittest.main()
-
